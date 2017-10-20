cb010ba0b4689374e73b446f72ee991cc6e3a61f
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2017-10-20 16:57:25 +0300

    pdf template with agreements

--- src/components/CreatePdf.vue
+++ src/components/CreatePdf.vue
@@ -17,6 +17,73 @@ export default {
       pdfFonts,
       docDefinition: {
         content: 'This is an sample PDF printed with pdfMake, тест'
+      },
+      docWithAgreement: {
+        content: [
+          {
+            text: 'АКТ',
+            style: 'header'
+          },
+          {
+            text: 'приема - передачи имущества\n\n',
+            style: 'smallheader'
+          },
+          {
+            text: 'от 13.09.2017 г\n\n',
+            alignment: 'right',
+            decoration: 'underline'
+          },
+          {
+            text: 'Стороны составили настоящий АКТ о том, что:'
+          },
+          {
+            text: 'Передает: Lead Engineer Nunc Mauris LLP Elmo Franklin\n\n',
+            decoration: 'underline'
+          },
+          {
+            text: 'Принимает: Lead Engineer Nunc Mauris LLP Elmo Franklin\n\n',
+            decoration: 'underline'
+          },
+          {
+            text: [
+              'во временное пользование в количестве ',
+              {text: ' 1 ', decoration: 'underline'},
+              ' единиц(ы) следующее имущество'
+            ]
+          },
+          {
+            text: 'iPad Air2 32GB(MNVR2RU/A)+Чехол',
+            decoration: 'underline'
+          },
+          {
+            text: 'Принимающая сторона обязуется:\n\n',
+            fontSize: 14,
+            bold: true
+          },
+          {
+            ol: [
+              'использовать имущество строго по прямому назначению в целях, предусмотренных спецификацией;',
+              'не передавать имущество в пользование третьим лицам (независимо от степени родства);',
+              'содержать имущество в исправности, соблюдать соответствующие стандарты, технические условия, и правила технической эксплуатации согласно инструкции предприятия-изготовителя;',
+              'не производить никаких конструктивных изменений (модификаций) имущества; а также нести перед OU OJSC «Company» ответственность за сохранность и техническое состояние полученного в пользование имущества;',
+              'возмещать ущерб, причиненный по вине принимающей стороны или третьих лиц, привлеченных принимающей стороной для работы с имуществом;',
+              'принимать меры по предотвращению утраты имущества, порчи и т.п.;',
+              'при выявлении фактов утраты, порчи, хищения, повреждения имущества своевременно сообщить в OU OJSC «Company»;'
+            ]
+          }
+        ],
+        styles: {
+          header: {
+            fontSize: 18,
+            bold: true,
+            alignment: 'center'
+          },
+          smallheader: {
+            fontSize: 16,
+            bold: true,
+            alignment: 'center'
+          }
+        }
       }
     }
   },
@@ -24,7 +91,7 @@ export default {
   methods: {
     create () {
       this.pdfMake.vfs = this.pdfFonts.pdfMake.vfs
-      this.pdfMake.createPdf(this.docDefinition).download('optionalName.pdf')
+      this.pdfMake.createPdf(this.docWithAgreement).download('optionalName.pdf')
     }
   }
 }
