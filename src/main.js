67d81fc923e0d1839c907f637460c07d4dfb735d
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2018-03-30 23:37:30 +0300

    vue version fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import store from './store'
@@ -7,12 +5,10 @@ import store from './store'
 import 'bulma/css/bulma.css'
 
 Vue.config.productionTip = false
-Vue.config.devtools = true
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   store,
-  template: '<App/>',
-  components: { App }
+  render: h => h(App)
 })
